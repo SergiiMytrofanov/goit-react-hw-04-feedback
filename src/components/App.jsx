21a6e55f1a02ff69d6ddce0a54c24396891ab63c
SergiiMytrofanov <122700399+SergiiMytrofanov@users.noreply.github.com>
2023-08-12 11:30:40 +0300

    Update App.jsx
    Refactoring to change props logic on hook princeple

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,27 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import FeedbackColector from "./Feedback/Feedback";
 import StatBlock from "./StatBlock/StatBlock";
 import Section from "./Section/Section";
 import Notification from "./StatBlock/Notіfication";
 import styles from "./App.module.css";
 
-class AppFeedbackStat extends Component {
-  state = {
+
+const AppFeedbackStat = () => {
+  const [feedback, setFeedback] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
-  };
+  });
 
-  handleFeedbackClick = (feedbackType) => {
-    this.setState((prevState) => ({
+  const handleFeedbackClick = (feedbackType) => {
+   setFeedback((prevState) => ({
       ...prevState,
       [feedbackType]: prevState[feedbackType] + 1,
     }));
   };
 
-  calculatePositiveRate = () => {
-    const { good, neutral, bad } = this.state;
+  const calculatePositiveRate = () => {
+    const { good, neutral, bad } = feedback;
     const totalFeedback = good + neutral + bad;
     if (totalFeedback === 0) {
       return 0;
@@ -28,20 +29,18 @@ class AppFeedbackStat extends Component {
     return (good / totalFeedback) * 100;
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-
-    const totalFeed = good + neutral + bad;
-    const positiveRate = this.calculatePositiveRate();
+  const { good, neutral, bad } = feedback;
+  const totalFeed = good + neutral + bad;
+  const positiveRate = calculatePositiveRate();
 
     return (
       <div className={styles.feedbackContainer}>
         <Section title="Вам сподобалося у нас &#9829;">
-          <FeedbackColector feedByClick={(type) => this.handleFeedbackClick(type)} />
+          <FeedbackColector feedByClick={(type) => handleFeedbackClick(type)} />
         </Section>
           {totalFeed === 0 ? (
             <Section>
-            <Notification message={"Тут ще немає відгуків. Ви перший"} />
+            <Notification message={"Тут ще немає відгуків. Ви перший:)"} />
             </Section>
           ) : (
             <Section title="Результат опитування">
@@ -57,7 +56,7 @@ class AppFeedbackStat extends Component {
 
       </div>
     );
-  }
+
 }
 
 export default AppFeedbackStat;
